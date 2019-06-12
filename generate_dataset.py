7ba21bc807a01bc463ebb02913122589dcc7d6ee
rubenseggers <rubenseggers@gmail.com>
2019-06-12 09:30:47 +0200

    generate dataset v1

--- generate_dataset.py
+++ generate_dataset.py
@@ -5,7 +5,7 @@
 # http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
 # Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
-"""Minimal script for reproducing the figures of the StyleGAN paper using pre-trained generators."""
+"""Minimal script for producing a dataset using StyleGAN."""
 
 import os
 import pickle
@@ -14,58 +14,57 @@ import PIL.Image
 import dnnlib
 import dnnlib.tflib as tflib
 import config
+from tqdm import tqdm
+import random
 
 
-url_ffhq = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ'  # karras2019stylegan-ffhq-1024x1024.pkl
 synthesis_kwargs = dict(output_transform=dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True), minibatch_size=8)
-_Gs_cache = dict()
 
 
-def load_Gs(url):
-    if url not in _Gs_cache:
-        with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
-            _G, _D, Gs = pickle.load(f)
-        _Gs_cache[url] = Gs
-    return _Gs_cache[url]
-
-
-def draw_variations_figure(png, Gs, w, h, src_seeds, dst_seeds, style_ranges):
-    print(png)
-    src_latents = np.stack(np.random.RandomState(seed).randn(Gs.input_shape[1]) for seed in src_seeds)
-    print("src: ", src_latents)
-    dst_latents = np.stack(np.random.RandomState(seed).randn(Gs.input_shape[1]) for seed in dst_seeds)
-    print("dst: ", dst_latents)
-    src_dlatents = Gs.components.mapping.run(src_latents, None)  # [seed, layer, component]
-    dst_dlatents = Gs.components.mapping.run(dst_latents, None)  # [seed, layer, component]
-    src_images = Gs.components.synthesis.run(src_dlatents, randomize_noise=False, **synthesis_kwargs)
-    dst_images = Gs.components.synthesis.run(dst_dlatents, randomize_noise=False, **synthesis_kwargs)
-
-    canvas = PIL.Image.new('RGB', (w * (len(src_seeds) + 1), h * (len(dst_seeds) + 1)), 'white')
-    for col, src_image in enumerate(list(src_images)):
-        canvas.paste(PIL.Image.fromarray(src_image, 'RGB'), ((col + 1) * w, 0))
-    for row, dst_image in enumerate(list(dst_images)):
-        canvas.paste(PIL.Image.fromarray(dst_image, 'RGB'), (0, (row + 1) * h))
-        row_dlatents = np.stack([dst_dlatents[row]] * len(src_seeds))
-        row_dlatents[:, style_ranges[row]] = src_dlatents[:, style_ranges[row]]
-        row_images = Gs.components.synthesis.run(row_dlatents, randomize_noise=False, **synthesis_kwargs)
-        for col, image in enumerate(list(row_images)):
-            canvas.paste(PIL.Image.fromarray(image, 'RGB'), ((col + 1) * w, (row + 1) * h))
-    canvas.save(png)
-
-
+# noinspection PyArgumentList
 def main():
+    # Initialize TensorFlow.
     tflib.init_tf()
-    os.makedirs(config.result_dir, exist_ok=True)
-    draw_variations_figure(os.path.join(config.result_dir, 'variations.png'),
-                           load_Gs(url_ffhq), w=1024, h=1024,
-                           src_seeds=[639, 701, 687, 615, 2268],
-                           dst_seeds=[888, 829, 1898, 1733, 1614, 845],
-                           style_ranges=[range(0, 4)]*3+[range(4, 8)]*2+[range(8, 18)])
 
-    # draw_style_mixing_figure(os.path.join(config.result_dir, 'figure03-style-mixing.png'), load_Gs(url_ffhq), w=1024, h=1024, src_seeds=[639,701,687,615,2268], dst_seeds=[888,829,1898,1733,1614,845], style_ranges=[range(0,4)]*3+[range(4,8)]*2+[range(8,18)])
-    # draw_noise_detail_figure(os.path.join(config.result_dir, 'figure04-noise-detail.png'), load_Gs(url_ffhq), w=1024, h=1024, num_samples=100, seeds=[1157,1012])
-    # draw_noise_components_figure(os.path.join(config.result_dir, 'figure05-noise-components.png'), load_Gs(url_ffhq), w=1024, h=1024, seeds=[1967,1555], noise_ranges=[range(0, 18), range(0, 0), range(8, 18), range(0, 8)], flips=[1])
+    # Create output dir.
+    os.makedirs(config.result_dir, exist_ok=True)
+
+    # Load pre-trained network.
+    url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ'  # karras2019stylegan-ffhq-1024x1024.pkl
+    with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
+        _G, _D, Gs = pickle.load(f)
+
+    # Parameters.
+    n_users = 5
+    n_per_user = 20
+    random.seed(420)
+
+    # src_seeds = [639, 701, 687, 615, 2268]
+    src_seeds = random.sample(range(10000), n_users)
+    user_base_latents = np.stack(np.random.RandomState(seed).randn(Gs.input_shape[1]) for seed in src_seeds)
+    user_base_dlatents = Gs.components.mapping.run(user_base_latents, None)  # [user, layer, component]
+
+    for person_id in tqdm(range(n_users)):
+        # Create variation.
+        user_all_dlatents = np.stack([user_base_dlatents[person_id]] * n_per_user)  # [variation, layer, component]
+        from_layer = 0
+        to_layer = 4
+        scale = 0.5
+        user_all_dlatents[:, range(from_layer, to_layer)] += (np.random.rand(n_per_user, len(range(from_layer, to_layer)), 512) - 0.5) * scale
+
+        # Generate images.
+        user_images = Gs.components.synthesis.run(user_all_dlatents, randomize_noise=False, **synthesis_kwargs)
+
+        # Save images.
+        for img_id in range(n_per_user):
+            png_filename = os.path.join(config.result_dir, 'person_{}-img_{}.png'.format(person_id, img_id))
+            # PIL.Image.fromarray(user_images[person_id], 'RGB').resize((64, 64)).save(png_filename)
+            PIL.Image.fromarray(user_images[img_id], 'RGB').save(png_filename)
 
 
 if __name__ == "__main__":
     main()
+
+# style_ranges = [range(0, 4)]*3+[range(4, 8)]*2+[range(8, 18)]
+# dst_seeds = [888, 829, 1898, 1733, 1614, 845]
+# dlatents[:, style_ranges[row]] = src_dlatents[:, style_ranges[row]]
